a1a3419474ee01eb4cb59bfd7aa569197243d5e3
epic2225 <69192198+epic2225@users.noreply.github.com>
2021-03-09 22:43:25 +0800

    Update CHandler.lua

--- CHandler.lua
+++ CHandler.lua
@@ -29,11 +29,21 @@ local CPS = {
     Settings = {
         FixturesEnabled = true; --Set to false to completely disable all fixture controllers
         FixturesUpdating = true; --Set to false to stop updating the attributes of the fixtures
-        EffectsUpdating = true; --Set to false to stop updating the effects
     };
     
     Fixtures = workspace.CLPakyScenius;
     Effects = workspace.Effects;
+
+    Attributes = {
+        Intensity = {default=0,min=0,max=255,main="Intensity"},
+        R = {default=255,min=0,max=255,main="R"},
+        G = {default=255,min=0,max=255,main="G"},
+        B = {default=255,min=0,max=255,main="B"},
+        Zoom = {default=200,min=0,max=255,main="Zoom"},
+        Pan = {default=0,min=-180,max=180,main="Pan"},
+        Tilt = {default=0,min=-180,max=180},main="Tilt",
+        Speed = {default=127,min=0,max=255,main="Speed"},
+    };
     
     FX = {
         Sine = {
@@ -121,13 +131,26 @@ local CPS = {
             };
         }
     };
+
+    Groups = {
+        "G1",
+        "G2",
+        "G3",
+        "G4",
+        "G5",
+        "G6",
+        "G7",
+        "G8",
+    };
+
+    updaters = {}
 }
 
 --Functions
 
 local debugMode = false
 
-local print = function(...) if (debugMode) then print(...) end end
+local dPrint = function(...) if (debugMode) then print(...) end end
 
 local reverse = function(bool)
     return ({
@@ -140,51 +163,20 @@ function CPS:FindEffect(effectIndex, subIndex)
     return self:FindEffect(effectIndex)[subIndex]
 end
 
-function CPS:UpdateEffect(effectIndex, bool)
-    self.Effects[effectIndex].Disabled = reverse(bool);
+function CPS:UpdateEffect(i, ii, bool)
+    self.FX[i][ii].enabled = bool
+    self.Effects[i][ii].Disabled = reverse(bool);
 end
 
 function CPS:ChangeEffectSpeed(index1, index2, spd)
-    if (index1 == "All") and (spd == nil) then
-        for _, v in pairs(self.FX) do
-            for _, vv in pairs(v) do
-                vv.Speed = index2
-            end
-        end
-    else
-        self.FX[index1][index2].Speed = spd
-    end
+    if not self.Effects[index1][index2].enabled 
 end
 
 function CPS:UpdateFixtures(group, attribute, value)
-    local split = function(s, x, y)
-        return s:sub(1, x), s:sub(y, s:len())
-    end
-    
-    if group == "All" then
-        local fixtures = self.Fixtures:GetChildren()
-        
-        for i = 1, #fixtures do
-            local fixtureGroup = fixtures[i]:GetChildren()
-            
-            for ii = 1, #fixtureGroup do
-                fixtureGroup[ii].Control[attribute].Value = value
-            end
-        end
-    else
-        local s1, s2 = split(group, 1, 2)
-        local groups = {p = group, s = s1 .. "R" .. s2}
-        
-        local gr1 = self.Fixtures[groups.p]:GetChildren()
-        local gr2 = self.Fixtures[groups.s]:GetChildren()
-        
-        for i = 1, #gr1 do
-            gr1[i].Control[attribute].Value = value
-        end
-        
-        for i = 1, #gr2 do
-            gr2[i].Control[attribute].Value = value
-        end
+    if self.Settings.FixturesUpdating then
+        local att = self.Attributes[attribute]
+
+        self.updaters[group](att, value)
     end
 end
 
@@ -196,11 +188,60 @@ function CPS:GetFixtures()
         local group = self.Fixtures[i]:GetChildren()
         
         for ii = 1, #group do
-            fixtures[i + (#group * (i - 1))] = group[ii]
+            fixtures[#fixtures + 1] = group[ii]
         end
     end
     
     return fixtures
+end
+
+function CPS:init()
+    local clamp = function(x, y, z)
+        return x >= z and z or x <= y and y or x
+    end
+
+    local split = function(s, x, y)
+        return s:sub(1, x), s:sub(y, s:len())
+    end
+
+    for _, v in pairs(self:GetFixtures()) do
+        if self.Settings.FixturesEnabled then
+            v.Control.Disabled = false
+            if self.Settings.FixturesUpdating then
+                for attribute, tbl in pairs(self.Attributes) do
+                    local value, args = clamp(tbl.default, tbl.min, tbl.max)
+                    
+                    self:UpdateFixtures("All", attribute, value)
+                end
+            end
+        end
+    end
+
+    self.updaters["All"] = function(att, value)
+        for _, v in pairs(self:GetFixtures()) do
+            v.Control[att.main].Value = clamp(value, att.min, att.max)
+        end
+    end
+    
+
+    for _, v in pairs(self.groups) do
+        if string.match(v, "G") then
+            self.updaters[v] = function(att, value)
+                local s1, s2 = split(group, 1, 2)
+                local GR1, GR2 = group, concat(s1, "R", s2))
+
+                local Value = clamp(value, att.min, att.max)
+
+                for _, v in pairs(self.Fixtures[GR1]:GetChildren()) do
+                    v.Control[attribute].Value = Value
+                end
+
+                for _, v in pairs(self.Fixtures[GR2]:GetChildren()) do
+                    v.Control[attribute].Value = Value
+                end
+            end
+        end
+    end
 end
 
 debug.profileend("CHandler")
