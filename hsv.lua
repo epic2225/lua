6279bdeadeeed1730ddaee62185b02c4b3c71a3c
epic2225 <69192198+epic2225@users.noreply.github.com>
2021-03-09 22:56:02 +0800

    Update hsv.lua

--- hsv.lua
+++ hsv.lua
@@ -1,44 +1,34 @@
 return function(h, sat, val)
-    local hue = (h % 360)
+    local v = (h % 360)
     
-    local function getRed(v)
-        local r, r2 = 0, 0
-        
-        if v <= 240 then r = 0
-        elseif v >= 300 then r = 1
-        else r = ((v - 240) / 60) end
-        if v <= 60 then r2 = 1
-        elseif v >= 120 then r2 = 0
-        else r2 = 1 - ((v - 60) / 60) end
-        
-        return math.floor((r + r2) * 255)
-    end
+    local r, r2 = 0, 0
+	local g, g2 = 0, 0
+	local b, b2 = 0, 0
+	
+	if v <= 240 then r = 0
+    elseif v >= 300 then r = 1
+    else r = ((v - 240) / 60) end
+    if v <= 60 then r2 = 1
+    elseif v >= 120 then r2 = 0
+    else r2 = 1 - ((v - 60) / 60) end
+	
+    if v <= 0 then g = 0
+    elseif v >= 60 then g = 1
+    else g = (v / 60) end
+    if v <= 180 then g2 = 1
+    elseif v >= 240 then g2 = 0
+    else g2 = 1 - ((v - 180) / 60) end
     
-    local function getGreen(v)
-        local g, g2 = 0, 0
+    if v <= 120 then b2 = 0
+    elseif v >= 180 then b2 = 1
+    else b2 = ((v - 120) / 60) end
+    if v >= 360 then b = 0
+    elseif v <= 300 then b = 1
+    else b = 1 - ((v - 300) / 60) end
         
-        if v <= 0 then g = 0
-        elseif v >= 60 then g = 1
-        else g = (v / 60) end
-        if v <= 180 then g2 = 1
-        elseif v >= 240 then g2 = 0
-        else g2 = 1 - ((v - 180) / 60) end
-        
-        return math.floor((g + g2) * 255) - 255
-    end
-    
-    local function getBlue(v)
-        local b, b2 = 0, 0
-        
-        if v <= 120 then b2 = 0
-        elseif v >= 180 then b2 = 1
-        else b2 = ((v - 120) / 60) end
-        if v >= 360 then b = 0
-        elseif v <= 300 then b = 1
-        else b = 1 - ((v - 300) / 60) end
-        
-        return math.floor((b + b2) * 255) - 255
-    end
+    local red = math.floor((r + r2) * 255)
+	local green = math.floor((g + g2) * 255) - 255
+	local blue = math.floor((b + b2) * 255) - 255
     
     local function mix(s, v, r, g, b)
         local clamp = function(x, y, z) if x < y then return y elseif x > z then return z else return x end end
@@ -62,5 +52,5 @@ return function(h, sat, val)
         return math.floor(val_R), math.floor(val_G), math.floor(val_B)
     end
     
-    return mix(sat, val, getRed(hue), getGreen(hue), getBlue(hue))
+    return mix(sat, val, red, green, blue)
 end
