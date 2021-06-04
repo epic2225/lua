77060307e34a37e05e0b37ba07778b8fdda6ddcb
epic2225 <69192198+epic2225@users.noreply.github.com>
2021-06-04 23:12:10 +0800

    Update visualSystem.lua

--- visualSystem.lua
+++ visualSystem.lua
@@ -4,130 +4,180 @@ visualSystem.center = workspace.CentrePart
 visualSystem.screens = workspace.Screens:GetChildren()
 visualSystem.panels = {}
 visualSystem.pack = script.Pack:GetChildren()
-visualSystem.screenLimit = 0
+visualSystem.maxScreens = 500
 visualSystem.const = 50
 
-visualSystem.gifs = {
+local self = {
 	storage = {},
 	running = {},
-	
-	maxFPS = 60,
 }
 
 local replicatedStorage = game:GetService("ReplicatedStorage")
 local httpService = game:GetService("HttpService")
 local runService = game:GetService("RunService")
 
-function copy(inTable, outTable)
-	for index, value in pairs(inTable) do
-		if type(v) == "table" then
-			outTable[index] = copy(value)
-		else
-			outTable[index] = value
-		end
-	end
-	return outTable
-end
+local runEvent = runService.Stepped
+
+local match = string.match
+local sub = string.sub
+
+local uDim2 = UDim2.new
+local vector2 = Vector2.new
+
+local floor = math.floor
 
 function visualSystem:mapVisuals(screen, center)
-	local d = screen.CFrame.Position - center.CFrame.Position
-	for _, v in pairs(screen.g.UI:GetChildren()) do
-		if v:IsA("ImageLabel" or "Frame" or "TextLabel") then
-			v.Position = UDim2.new(0.5, -(d.X * self.const), 0.5, d.Y * self.const)
-		elseif v:IsA("Folder") and string.match(v.Name, "SPRITE") then
+	local gui = screen.g
+	
+	local const = self.const
+	local c0 = screen.CFrame
+	local c1 = center.CFrame
+	local difference = c0.Position - c1.Position
+	local x, y = difference.x * const, difference.y * const
+	local size = screen.Size
+	local sizeX, sizeY = size.X * const, size.Y * const
+	
+	for _, v in pairs(gui.UI:GetChildren()) do
+		if v.ClassName == "ImageLabel" or "Frame" or "TextLabel" then
+			v.Position = uDim2(0.5, -y, 0.5, x)
+		elseif v.ClassName == "Folder" and sub(v.Name, 1, 3) == "GIF" then
 			for _, frame in pairs(v:GetChildren()) do
-				if frame:IsA("TextLabel" or "ImageLabel") then
-					frame.Position = UDim2.new(0.5, -(d.X * self.const), 0.5, d.Y * self.const)
+				if frame.ClassName == "TextLabel" or "ImageLabel" then
+					frame.Position = uDim2(0.5, -x, 0.5, y)
 				end
 			end
 		end
 	end
-	screen.g.CanvasSize = Vector2.new(screen.Size.X * self.const, screen.Size.Y * self.const)
+	
+	gui.CanvasSize = vector2(sizeX, sizeY)
 end
 
 function visualSystem:getPanels()
 	local panels = {}
-	for k, v in pairs(self.panels) do
-		local panel = {name = nil, screens = {}}
+	local __panels = self.panels
+	
+	for k, v in pairs(__panels) do
+		local panel = {screens = {}}
+		panel.name = v.Name
+		
+		local screens = panel.screens
+		
 		for _, ui in pairs(v.g.UI:GetChildren()) do
-			panel.name = k
-			panel.screens[ui.name] = ui
+			screens[ui.Name] = ui
 		end
+		
 		panels[panel.name] = panel
 	end
+	
 	return panels
 end
 
-function visualSystem.sprites.new(settings)
-	local self = {}
+function visualSystem.newGif(settings)
+	local new = {
+		fps = settings.fps,
+		id = settings.id,
+		name = settings.name,
+		
+		frames = {},
+	}
 	
-	self.__type = "sprite"
-	
-	self.folder = settings.frames
-	self.id = settings.id
-	self.fps = settings.fps
-	
-	self.frames = {}
+	local frames = new.frames
+	local id = new.id
+	local amount = #frames
 	
 	for _, v in pairs(visualSystem:getPanels()) do
-		self.frames[#self.frames + 1] = v.screens[self.folder]
+		frames[#frames + 1] = v.screens[frames]
 	end
 	
-	local sprite = {sprite = self, connection = nil}
-	
-	if settings.alreadyRunning == true then
-		visualSystem.sprites:start(sprite)
-	end
-	
-	return self
-end
-
-function visualSystem.sprites:start(sprite)
-	if not sprite then return end
-	if self.storage[sprite.id] then return end
-	
-	self.sprites[settings.id].connection = runService.Stepped:Connect(function()
-		new.x += (1 / (self.maxFPS / math.clamp(sprite.fps, 1, self.maxFPS)))
-		for i = 1, #sprite.frames do
-			if sprite.frames[i]:IsA("Folder") then
-				local f = sprite.frames[i]:GetChildren()
-				local amt = #f
-				for ii = 1, amt do
-					local x = (math.floor(new.x) % amt) + 1
-					if x > 2 then
-						f[x - 1].Visible = false
+	new.run = function()
+		local x = 0
+		local fps = new.fps
+		
+		self.running[id] = {new, runEvent:Connect(function()
+			x += 1 / (60 / fps)
+			for i = 1, amount do
+				local f = frames[i]
+				if f.ClassName == "Folder" then
+					local v = f:GetChildren()
+					local amt = #v
+					for ii = 1, amt do
+						local __x = (floor(x) % amt) + 1
+						if __x > 2 then
+							v[__x - 1].Visible = false
+						end
+						if ii == amt then
+							v[amt].Visible = false
+						end
+						v[__x].Visible = true
 					end
-					if ii == #f then
-						f[amt].Visible = false
-					end
-					f[x].Visible = true
 				end
 			end
-		end
-	end)
+		end)}
+	end
+	
+	new.stop = function()
+		self.running[id][2] = nil
+	end
 	
 	return new
 end
 
-function visualSystem:init(screenLimit)
-	local range = #self.screens
+function visualSystem.runGif(id)
+	local running = self.running
+	local storage = self.storage
 	
-	self.screenLimit = screenLimit
-	
-	if range <= self.screenLimit then
-		self.amountOfScreens = range
-		for i = 1, range do
-			self.panels[i] = self.screens[i]
-			for ii = 1, #self.pack do
-				local new = self.pack[ii]:Clone()
-				new.Parent = self.panels[i].g.UI
-			end
-		end
-		wait()
-		for _, v in pairs(self.panels) do
-			self:mapVisuals(v, self.center)
+	if not running[id] then
+		local stored = storage[id]
+		if stored then
+			running[id] = stored
 		end
 	end
+end
+
+function visualSystem.pauseGif(id)
+	local running = self.running
+	local storage = self.storage
+	
+	local gif = running[id]
+	
+	if gif then
+		gif[2].stop()
+		
+		if storage[id] then
+			running[id] = nil
+		end
+	end
+end
+
+function visualSystem:init()
+	local screens = self.screens
+	local panels = self.panels
+	local maxScreens = self.maxScreens
+	local pack = self.pack
+	local center = self.center
+	local range, pRange = #screens, #pack
+	
+	if range <= maxScreens then
+		for i = 1, range do
+			panels[i] = screens[i]
+			
+			local panel = panels[i]
+			local UI = panel.g.UI
+			
+			for ii = 1, pRange do
+				local new = pack[ii]:Clone()
+				new.Parent = UI
+			end
+		end
+		
+		wait()
+		
+		for _, v in pairs(panels) do
+			self:mapVisuals(v, center)
+		end
+	end
+	
+	self.amountOfScreens = range
 end
 
 return visualSystem
